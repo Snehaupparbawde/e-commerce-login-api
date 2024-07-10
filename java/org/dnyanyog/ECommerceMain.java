b2aba7cb69bea494eca4dc705f1f8874582246b7
Snehaupparbawde <115337869+Snehaupparbawde@users.noreply.github.com>
2024-07-10 23:19:39 +0530

    ECommerce Login Controller using SpringBoot

---
+++ java/org/dnyanyog/ECommerceMain.java
@@ -0,0 +1,12 @@
+package org.dnyanyog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMain {
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMain.class, args);
+	}
+
+}
